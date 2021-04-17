77b67a5fc0e77f4a15a70fcc23004f3fb01de4b8
blessed-19901108 <77671405+blessed-19901108@users.noreply.github.com>
2021-04-17 19:36:31 +0800

    Create Shape.java

---
+++ shapes/Shape.java
@@ -0,0 +1,11 @@
+package shapes;
+
+import java.awt.Graphics;
+/*Graphics类提供基本绘图方法，Graphics类提供基本的几何图形绘制方法，
+ *主要有：画线段、画矩形、画圆、画带颜色的图形、画椭圆、画圆弧、画多边形、画字符串等*/
+
+public abstract class Shape {
+	
+	public abstract void draw(Graphics g);
+	
+}
