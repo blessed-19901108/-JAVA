1a8e6b6f0f11b7493b9695779b1486194b0b925b
blessed-19901108 <77671405+blessed-19901108@users.noreply.github.com>
2021-04-19 20:01:23 +0800

    Update Field.java

--- fox_and_rabbit/field/Field.java
+++ fox_and_rabbit/field/Field.java
@@ -8,7 +8,7 @@ public class Field {
   private int width;
   private int height;
   private Cell[][] field;// 声明有这么个数组变量,但未承接任何对象
-  private static final Location[] adjacent = { 
+  private static final Location[] adjacent = { //adjacent:邻近的
     new Location(-1, -1), new Location(-1, 0), new Location(-1, 1),
     new Location(0, -1), new Location(0, 0), new Location(0, 1),
     new Location(1, -1), new Location(1, 0),new Location(1, 1),
@@ -39,6 +39,7 @@ public class Field {
     return field[row][col];
   }
 
+  /**和下面getFreeNeighbor一样，只不过if()的条件不一样*/
   public Cell[] getNeighbor(int row, int col) {
     ArrayList<Cell> list = new ArrayList<Cell>();
     for (int i = -1; i < 2; i++) {
@@ -55,6 +56,8 @@ public class Field {
 
   public Location[] getFreeNeighbor(int row, int col) {
     ArrayList<Location> list = new ArrayList<Location>();
+    /**下面这个for循环，首先本方法传入一个坐标，以该坐标为原点，向周边移动就可得到邻近得到坐标（+1或者-1）
+     * 再将空的临近的坐标加到list中，再将容器类的list转化为数组类的list。*/
     for (Location loc : adjacent) {
       int r = row + loc.getRow();
       int c = col + loc.getCol();
@@ -70,6 +73,7 @@ public class Field {
    * 对于第一个重载方法，是将list直接转为Object[] 数组；
    * 第二种方法是将list转化为你所需要类型的数组，当然我们用的时候会转化为与list内容相同的类型。*/
 
+  /**安排一个随机的空的位置*/
   public boolean placeRandomAdj(int row, int col, Cell cell) {
     boolean ret = false;
     Location[] freeAdj = getFreeNeighbor(row, col);
@@ -89,6 +93,7 @@ public class Field {
     }
   }
 
+  /**如果被吃了就被清除*/
   public void remove(Cell fed) {
     for (int row = 0; row < height; row++) {
       for (int col = 0; col < width; col++) {
@@ -100,12 +105,14 @@ public class Field {
     }
   }
 
+  /**删除原来在格子里的生物，同时让一个Cell类的变量指向那个格子，即使Cell的对象还没有被创建出来*/
   public Cell remove(int row, int col) { // *函数重构
     field[row][col] = null;
     Cell ret = field[row][col];
     return ret;
   }
 
+  /**让field[loc.getRow()][loc.getCol()]这个变量指向field[row][col]*/
   public void move(int row, int col, Location loc) {
     field[loc.getRow()][loc.getCol()] = field[row][col];
     remove(row, col);
