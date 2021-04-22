4697d6a4b930869ee7a65342e0aa46d123747165
blessed-19901108 <77671405+blessed-19901108@users.noreply.github.com>
2021-04-22 22:13:23 +0800

    内部类的应用
    首先,将Handler中doCmd改成抽象方法,而且无需传递game对象进入
    
    public abstract class Handler {
    	//构造抽象方法
    	public abstract void doCmd(String word);
    	public boolean isBye() {
    		return false;
    	};
    }
    
    继承Handler类的三个方法类依次改为:
    
    public class HandlerGo extends Handler {
    	@Override
    	public void doCmd(String word) {}
    }
    public class HandlerHelp extends Handler {
    	@Override
    	public void doCmd(String word) {
    		System.out.print("迷路了吗？你可以做的命令有：go bye help");
            System.out.println("如：\tgo east");
    	}
    }
    public class HandlerBye extends Handler {
    	@Override
    	public boolean isBye() {
    		return true;
    	}
    	@Override
    	public void doCmd(String word) {
    	}
    }
    
    然后就可以给Game类重构:因为只有HandlerGo方法需要调用Game类中的goRoom()方法,所以其它两个类并不需要作为匿名内部类重构
    
        private static HashMap<String, Handler> handlers = new HashMap<String, Handler>();       
        public Game() 
        {
        	handlers.put("bye",  new HandlerBye());
        	handlers.put("help", new HandlerHelp());
        	//匿名内部类,重构HandlerGo类doCmd()方法
        	handlers.put("go", new HandlerGo(){
    			@Override
    			public void doCmd(String word) {
    				goRoom(word);
    			}
        	});
            createRooms();
        }
    
    
    
    综上:使用内部类以后即解除了Handler对Game类的依赖,又提高了代码的可读性

--- castle/castle/Handler.java
+++ castle/castle/Handler.java
@@ -3,11 +3,25 @@ package castle;
 public class Handler {
 	protected Game game;
 	
+	//无内部类时
+	/*
 	public Handler(Game game)//构造函数构造对象时就记下game，是handlergo要用到的
 	{
 		this.game = game;
 	}
 	public void doCmd(String word) {}//接受一个字符串输入，提供一个基础
+	*/
+	
+	//有内部类时-->首先,将Handler中doCmd改成抽象方法,而且无需传递game对象进入
+	public abstract class Handler {
+	//构造抽象方法
+	public abstract void doCmd(String word);
+	public boolean isBye() {
+		return false;
+		};
+	}
+	
+	
 	public boolean isBye() { return false;}
 	
 	public static void main(String[] args) {
