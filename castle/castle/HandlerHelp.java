7d2208c803b97d882f9ae8d48819c8391fdca647
blessed-19901108 <77671405+blessed-19901108@users.noreply.github.com>
2021-04-22 22:20:47 +0800

    内部类的应用
    1、首先,将Handler中doCmd改成抽象方法,而且无需传递game对象进入。
    2、继承Handler类的三个方法类依次改为:
    
    public class HandlerGo extends Handler {
    	@Override
    	public void doCmd(String word) {}
    }
    public class HandlerHelp extends Handler {
    	@Override
    	public void doCmd(String word) {
    		System.out.print("迷路了吗？你可以做的命令有：go bye help");
            System.out.println("如：\tgo east");
    	}
    }
    public class HandlerBye extends Handler {
    	@Override
    	public boolean isBye() {
    		return true;
    	}
    	@Override
    	public void doCmd(String word) {
    	}
    }

--- castle/castle/HandlerHelp.java
+++ castle/castle/HandlerHelp.java
@@ -1,10 +1,17 @@
 package castle;
 
 public class HandlerHelp extends Handler{
+	
+	//无内部类时
+	/*
 	public HandlerHelp(Game game)
 	{
 		super(game);
 	}
+	*/
+	
+	//有内部类时
+	/**不用重新构建方法*/
 	@Override
 	public void doCmd(String word) {
 		System.out.println("迷路了吗？你可以做的命令有：");
