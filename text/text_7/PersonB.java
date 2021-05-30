fe5d5c97e789eef36e4268ba2eca8d784327abb2
blessed-19901108 <77671405+blessed-19901108@users.noreply.github.com>
2021-05-30 23:23:14 +0800

    Create PersonB.java

---
+++ text/text_7/PersonB.java
@@ -0,0 +1,28 @@
+package Text_7;
+
+public class PersonB {
+	String name;
+	int age;
+	
+	/*public PersonB(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+
+	public PersonB(String name) {
+		super();
+		this.name = name;
+	}
+
+	public PersonB() {
+		super();
+	}
+	*/
+	public void introduce()
+	{
+		System.out.println("我是"+name+"，今年"+age+"岁。");
+	}
+	
+
+}
