c8882e2966ca69ee37c6c819cc0a6454b02c0c66
blessed-19901108 <77671405+blessed-19901108@users.noreply.github.com>
2021-05-30 23:23:45 +0800

    Create StudentB.java

---
+++ text/text_7/StudentB.java
@@ -0,0 +1,22 @@
+package Text_7;
+
+public class StudentB extends PersonB {
+	int a;
+
+	public StudentB() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	/*public StudentB()
+	{
+		super();//不写的话默认调用父类的无参
+		System.out.println("StudentB() is called.");
+	}
+	 
+	public StudentB(String name,int age)
+	{
+		super(name,age);//这是作为方法来用，传递的是实参-->name;String name-->是定义参数
+	}*/
+}
